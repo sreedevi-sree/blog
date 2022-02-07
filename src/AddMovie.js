31ef86733e857a30910f30d1f0fcee7eb427250c
sreedevi-sree <sreedevifsd@gmail.com>
2022-02-07 22:17:56 +0530

    first commit

--- src/AddMovie.js
+++ src/AddMovie.js
@@ -1,136 +1,57 @@
-
-// formik for form validation
-import { useFormik } from "formik";
-
-// yup is used for validate condition
-import * as yup from "yup";
-
-// import { useState } from "react";
+import { useState } from "react";
 import { useHistory } from "react-router-dom";
 
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 
-const formValidationSchema = yup.object({
-  name:yup
-  .string()
-  .required(),
-  
-  poster:yup
-  .string()
-  .min(4)
-  .required(),
-
-  rating:yup
-  .number()
-  .required(),
-
-  year:yup
-  .string()
-  .max(4)
-  .required(),
-
-  summary:yup
-  .string()
-  .min(20,"type min 20 chars")
-  .required(),
-
-  trailer:yup
-  .string()
-  .min(4)
-  .required(),
-});
 export function AddMovie() {
-  const {handleSubmit,values,handleChange,handleBlur,errors,touched}=
-    useFormik({
-        initialValues:{name:"",poster:"",rating:"",year:"",summary:"",trailer:""},
-        validationSchema:formValidationSchema,
-        onSubmit:(values)=>{
-            console.log("onSubmit", values);
-        }
-    })
-  
+  const [name, setName] = useState(" ");
+  const [poster, setPoster] = useState(" ");
+  const [rating, setRating] = useState(" ");
+  const [year, setYear] = useState(" ");
+  const [summary, setSummary] = useState(" ");
+  const [trailer, setTrailer] = useState(" ");
 const history=useHistory();
   return (
     <div className="MovieArray">
-      <form onSubmit={handleSubmit}>
-      <TextField
-    id="name"
-    name="name"
-    value={values.name}
-    onChange={handleChange}
-    onBlur={handleBlur}
-    type="name"
-    placeholder="name"
-    label="Name"
-     variant="outlined"
-      />
-{errors.name && touched.name ? errors.name : ""}
+    <TextField
+      value={name}
+      onChange={(event) => setName(event.target.value)}
+      id="outlined-basic" label="Name" variant="outlined" />
 
     <TextField
-      id="poster"
-      name="poster"
-      value={values.poster}
-      onChange={handleChange}
-      onBlur={handleBlur}
-      type="poster"
-      placeholder="poster"
-      label="Poster" variant="outlined" />
-{errors.poster && touched.poster ? errors.poster : ""}
+      value={poster}
+      onChange={(event) => setPoster(event.target.value)}
+      id="outlined-basic" label="Poster" variant="outlined" />
 
     <TextField
-      id="rating"
-      name="rating"
-      value={values.rating}
-      onChange={handleChange}
-      onBlur={handleBlur}
-      type="rating"
-      placeholder="rating"
-      label="Rating" variant="outlined" />
-{errors.rating && touched.rating ? errors.rating : ""}
+      value={rating}
+      onChange={(event) => setRating(event.target.value)}
+      id="outlined-basic" label="Rating" variant="outlined" />
 
     <TextField
-     id="year"
-     name="year"
-     value={values.year}
-     onChange={handleChange}
-     onBlur={handleBlur}
-     type="year"
-     placeholder="year"
-     label="Year" variant="outlined" />
-{errors.year && touched.year ? errors.year : ""}
+      value={year}
+      onChange={(event) => setYear(event.target.value)}
+      id="outlined-basic" label="Year" variant="outlined" />
 
     <TextField
-      id="summary"
-      name="summary"
-      value={values.summary}
-      onChange={handleChange}
-      onBlur={handleBlur}
-      type="summary"
-      placeholder="summary"
-      label="Summary" variant="outlined" />
-{errors.summary && touched.summary ? errors.summary : ""}
+      value={summary}
+      onChange={(event) => setSummary(event.target.value)}
+      id="outlined-basic" label="Summary" variant="outlined" />
 
       <TextField
-        id="trailer"
-        name="trailer"
-        value={values.trailer}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        type="trailer"
-        placeholder="trailer"
-       label="trailer" variant="outlined" />
-        {errors.trailer && touched.trailer ? errors.trailer : ""}
+        value={trailer}
+        onChange={(event) => setTrailer(event.target.value)}
+        id="outlined-basic" label="trailer" variant="outlined" />
     <Button
-   
       onClick={() => {
         const newMovie = {
-          name: values.name,
-          poster:values.poster,
-          rating: values.rating,
-          year: values.year,
-          summary:values.summary,
-          trailer:values.trailer
+          name: name,
+          poster: poster,
+          rating: rating,
+          year: year,
+          summary: summary,
+          trailer:trailer
         };
         fetch("https://618fa736f6bf4500174849a7.mockapi.io/movies", {
     method:"POST",
@@ -140,11 +61,7 @@ const history=useHistory();
     }
   }).then(()=>history.push("/movies"));
 }}
-type="submit"
       variant="outlined">Add Movie</Button>
-      </form>
-    
   </div>
   )
 }
-
