5b7780de187146715aa6a1c9d0d47c39a5197ce5
sreedevi-sree <sreedevifsd@gmail.com>
2022-02-08 18:50:26 +0530

    first commit

---
+++ src/BasicForm.js
@@ -0,0 +1,53 @@
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+const formValidationSchema = yup.object({
+    email:yup
+    .string()
+    .min(5)
+    .matches(
+        /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+        "please enter correct email format"
+        ),
+    
+    password:yup
+    .string()
+    .min(8)
+    .max(12)
+    .required(),
+});
+export function BasicForm(){
+    const {handleSubmit,values,handleChange,handleBlur,errors,touched}=
+    useFormik({
+        initialValues:{email:"",password:""},
+        validationSchema:formValidationSchema,
+        onSubmit:(values)=>{
+            console.log("onSubmit", values);
+        }
+    })
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+            id="email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            type="email"
+            placeholder="Email"
+            />
+            {errors.email && touched.email ? errors.email : ""}
+            <input
+            id="password"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            type="password"
+            placeholder="password"
+            />
+           {errors.password && touched.password ? errors.password : ""}
+<buttton type="submit">Submit</buttton>
+        </form>
+    )
+}
